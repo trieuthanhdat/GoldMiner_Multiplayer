640b14f3c080872108b68e3ce177c164067d1b6d
“TrieuThanhDat” <dat.tt@amanotes.com>
2024-02-07 13:05:26 +0700

    Revert "[UPDATE] - re-correct all .meta file + android.meta + androidXML.met"
    This reverts commit bcd5804f28f4cd7d650840586fdb36ef9d4cc0a9.

---
+++ Assets/Plugins/Demigiant/DOTweenPro/DOTweenAnimation.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:01ffa76cf5e61524d0a5da9cab8341b64f26e97fa421cf48597911f758b7d546
+size 36711
---
+++ Assets/Plugins/Demigiant/DOTweenPro/Editor/DOTweenAnimationInspector.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:afabd75df13bf22e55aa8d48aa82a3a06c7807c6df76d0aeb8f5d6baa878b342
+size 39182
